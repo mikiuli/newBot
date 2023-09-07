eaa3902c57fe366160378fc95f32672fb727738a
Iuliia Mishchenko <mdfrdlmfalknkf@yandex.ru>
2023-09-07 21:15:41 +0300

    create an instance of Config class with database connection and telegram data using invironment variables, get them through bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,11 @@
-print('hello')
+from config import load_config
+
+config = load_config(path=None)
+
+print('bot_token = ', config.tg_bot.token)
+print('admin_ids = ', config.tg_bot.admin_ids)
+print('\n')
+print('db_name = ', config.db.database)
+print('db_host = ', config.db.db_host)
+print('db_user = ', config.db.db_user)
+print('db_password = ', config.db.db_password)
